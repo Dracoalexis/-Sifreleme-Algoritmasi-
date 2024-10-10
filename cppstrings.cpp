24ef34b18f61eb5506e72fbfe125f1ea4e8c6be2
Anıl Emre Akkoç <136308720+Dracoalexis@users.noreply.github.com>
2024-10-10 11:01:56 +0300

    Update cppstrings.cpp

--- cppstrings.cpp
+++ cppstrings.cpp
@@ -9,10 +9,13 @@ int main(){
 
     string entered_string{};
     cout<<"Enter an string"<<endl;
-    cin>>entered_string;
+    getline(cin, entered_string);
 
     for(int i{0};i< entered_string.length();i++){
 
+        if(entered_string[i] == ' '){
+            continue;
+        }
         int cin_idx = alphabet.find(entered_string[i]); // 5
         int new_idx = alphabet.length() - cin_idx - 1; // 20
         entered_string[i] = alphabet[new_idx];
